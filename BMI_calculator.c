a5ab51ca394edaa086f7e9e8cc4eae5d271539bc
AryanGarg-beep <aryangarg3706@gmail.com>
2024-09-06 01:30:22 +0530

    logic

--- BMI_calculator.c
+++ BMI_calculator.c
@@ -10,5 +10,20 @@ int main()
     BMI = weight / (height * height);
     
     printf("\nYour BMI is: %f", BMI);
+    if(BMI < 18.5){
+        printf("\nYou are overweight");
+    }
+
+    else if(BMI >=18.5 && BMI <=25){
+        printf("\nYou have a healthy weight");
+    }
+
+    else if(BMI > 25 && BMI <=30){
+        printf("\nYou are overweight");
+    }
+
+    else{
+        printf("\nYou are obese");
+    }
  
 }
